fb86d067cd078bde06618df12243a947f7c962e3
Lokesh <luckylokeshsharma1111@gmail.com>
2024-05-31 01:03:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [flag, setFlag] = useState(false);
+  const [inputTask, setInputTask] = useState('');
+  const [inputDescription, setInputDesciption] = useState([])
+
+  const addTask = (title, description) => {
+    setTasks([...tasks, { id: Date.now(), title, description, status: 'pending' }]);
+  };
+
+  const moveTask = (id, newStatus) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id
+          ? {
+            ...task,
+            status: newStatus,
+            completedAt: newStatus === 'completed' ? new Date() : task.completedAt,
+          }
+          : task
+      )
+    );
+  };
+
+  const handleClick = () => {
+    addTask(inputTask, inputDescription);
+    setFlag(!flag);
+    setInputTask('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="section">
+        <h2>TO DO</h2>
+        <ul>
+          {tasks.filter((task) => task.status === 'pending').map((task) => (
+            <li key={task.id} className='tasks'>
+              <div className="task">
+                <span className="task-title">{task.title}</span>
+                <div className="task-buttons">
+                  <button onClick={() => moveTask(task.id, 'in-progress')}>Start</button>
+                </div>
+              </div>
+              <div style={{ wordWrap: "break-word", wordBreak: 'break-all', whiteSpace: 'normal' }}>
+                {task.description}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => setFlag(!flag)}>Create Issue</button>
+        {flag &&
+          <div className='inputs'>
+            <input
+              autoFocus
+              placeholder='New Task'
+              onChange={(e) => setInputTask(e.target.value)}
+              type='text'
+              value={inputTask}
+              required
+              style={{ marginBottom: "0.3rem" }}
+            />
+            <input
+              placeholder='description'
+              type='text'
+              style={{ marginBottom: "0.3rem" }}
+              onChange={(e) => setInputDesciption(e.target.value)}
+            />
+            <button onClick={handleClick}>Create</button>
+          </div>
+        }
+      </div>
+      <div className="section in-progress">
+        <h2>IN PROGRESS</h2>
+        <ul>
+          {tasks.filter((task) => task.status === 'in-progress').map((task) => (
+            <li key={task.id} className="tasks">
+              <div className='task'>
+                <span className="task-title">{task.title}</span>
+                <div className="task-buttons">
+                  <button onClick={() => moveTask(task.id, 'completed')}>Complete</button>
+                </div>
+              </div>
+              <div style={{ wordWrap: "break-word", wordBreak: 'break-all', whiteSpace: 'normal' }}>
+                {task.description}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="section done">
+        <h2>DONE</h2>
+        <ul>
+          {tasks.filter((task) => task.status === 'completed').map((task) => (
+            <li key={task.id} className="tasks">
+              <div className='task'>
+                <span className="task-title">
+                  {task.title}<span className="timestamp"> - {task.completedAt.toLocaleString()}</span>
+                </span>
+              </div>
+              <div style={{ wordWrap: "break-word", wordBreak: 'break-all', whiteSpace: 'normal',textAlign:'justify' }}>
+                {task.description}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
